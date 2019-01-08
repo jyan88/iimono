2f7369a998b543651bbf6073e20a973743920b49
jyan88 <hiroki7255@yahoo.co.jp>
2019-01-08 23:55:42 +0900

    userテーブル実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def new
   end
